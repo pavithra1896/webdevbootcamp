235ed64e18840398b64e5fdce9d3b6d1caafe5f8
Pavithra K <43128258+pavithra1896@users.noreply.github.com>
2019-08-06 16:19:14 +0530

    Update post.js

--- Associations/models/post.js
+++ Associations/models/post.js
@@ -1,3 +1,4 @@
+//using these models in separate files to shorten references.js code
 var mongoose = require("mongoose");
 
 // POST - title, content
@@ -6,3 +7,8 @@ var postSchema = new mongoose.Schema({
    content: String
 });
 module.exports = mongoose.model("Post", postSchema);
+//think of module as a return value for a file.So just like with the return value in a function nothing is returned unless
+//we explicitly tell javascript what we want to return out of the function.
+//Same thing with the file if we just include the file but we don't export anything out of it.
+//We'll actually be including Nothing will be requiring just an empty file.
+//What we need to do is export something and what we want to export is the model.
